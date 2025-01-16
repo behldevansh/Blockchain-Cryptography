aea4713f443b0370ebfe8a79503859cde9e479ff
Devansh Behl <heydevanshbehl@gmail.com>
2025-01-17 02:12:39 +0530

    add : owner describe

--- contracts/Chat.sol
+++ contracts/Chat.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract Chat is ERC721 {
+    address public owner;
 
 
      constructor(string memory _name, string memory _symbol)
         ERC721(_name, _symbol)
     {
-        // owner = msg.sender;
+        owner = msg.sender;
     }
 
 
--- test/Chat.js
+++ test/Chat.js
@@ -1,27 +1,26 @@
 const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether');
+  return ethers.utils.parseUnits(n.toString(), "ether");
 };
 
 describe("Chat", function () {
+  let deployer, user;
+  let chat;
 
-  let deployer, user
-  let chat
-
-  const NAME = "Chat"
-  const SYMBOL = "DC"
+  const NAME = "Chat";
+  const SYMBOL = "DC";
 
   beforeEach(async () => {
+    // Setup accounts
+    [deployer, user] = await ethers.getSigners()
+
     const Chat = await ethers.getContractFactory("Chat");
     chat = await Chat.deploy(NAME, SYMBOL);
-  })
-
-  
+  });
 
   describe("Deployment", function () {
     it("Sets the name", async () => {
-
       // Fetch name
       let result = await chat.name();
       // Check name
@@ -29,12 +28,14 @@ describe("Chat", function () {
     });
 
     it("Sets the symbol", async () => {
-
-
       // Fetch symbol
       let result = await chat.symbol();
       // Check symbol
       expect(result).to.equal(SYMBOL);
+      it("Sets the owner", async () => {
+        const result = await chat.owner()
+        expect(result).to.equal(deployer.address)
+      })
     });
   });
 });
